807df30bedcd82e8b8ace802565cce942f908204
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-05-15 23:27:26 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-color": "#00B98E",
+        "custom-color-2": "#0E2E50",
+      },
+    },
+  },
+  plugins: [],
+};
