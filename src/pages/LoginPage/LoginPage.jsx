f491cb97ad7b60676497d58439b3eb3c87508eaa
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-06-24 21:35:46 +0600

    test authentication details added in login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,6 +4,13 @@ import LoginAnimation from "../../components/Login/LoginAnimation";
 const LoginPage = () => {
   return (
     <div className="flex items-center gap-10 justify-center">
+      <div className="p-4 text-sm border border-teal-500 my-10">
+        <h1>TEST AUTHENTICATION DETAILS</h1>
+        <p>Guest: testuser1@gmail.com</p>
+        <p>Host: testhost@gmail.com</p>
+        <p>Admin: admin1@gmail.com</p>
+        <p>Pass: 12345678</p>
+      </div>
       <LoginAnimation />
       <Login />
     </div>
